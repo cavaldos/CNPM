781d2a3464322a9063d19f505b5934caf4bde11d
Thien Ly <vhtly@fit.edu.vn>
2025-03-21 14:35:35 +0700

    language service

--- client/src/hooks/LanguageSevice.jsx
+++ client/src/hooks/LanguageSevice.jsx
@@ -1,13 +1,27 @@
 
+import translate from '@iamtraction/google-translate';
+async function translateWord(word, targetLanguage) {
+    try {
+        // Kiểm tra nếu từ gốc hoặc ngôn ngữ đích không được cung cấp
+        if (!word || !targetLanguage) {
+            return "Vui lòng cung cấp từ gốc và ngôn ngữ đích!";
+        }
 
-const LanguageSwitcher = (language) => {
+        // Dịch từ
+        const result = await translate(word, { to: targetLanguage });
+        return result.text;
+    } catch (error) {
+        console.error("Lỗi khi dịch:", error);
+        return "Đã xảy ra lỗi khi dịch từ!";
+    }
+}
 
 
-    return language
-
-
+const LanguageSwitcher = async (word) => {
+    let nationality = "vi"
+    const translated = await translateWord(word, nationality);
+    return translated;
 };
-
 
 export default LanguageSwitcher;
 
