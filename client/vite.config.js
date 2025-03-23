8e6c821a836a8d3178fbb2d3354b4c2bcb326543
Thien Ly <vhtly@fit.edu.vn>
2025-03-23 21:51:30 +0700

    fix, update tailwind

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import reactRefresh from "@vitejs/plugin-react-refresh";
+import tailwindcss from '@tailwindcss/vite'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), reactRefresh()],
+  plugins: [react(), reactRefresh(), tailwindcss()],
   entry: "./src/index.js",
   server: {
     host: "0.0.0.0", // default: 'localhost'
